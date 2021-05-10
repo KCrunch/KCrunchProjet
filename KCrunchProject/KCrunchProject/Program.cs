7914eee42e17c57ff40e09b31ab88502a1ff3080
Shugo Tenchi <MLKwi6057@gmail.com>
2021-05-10 09:17:20 +0200

    chagement sur hellooo

---
+++ KCrunchProject/KCrunchProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KCrunchProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helloo World!");
+        }
+    }
+}
