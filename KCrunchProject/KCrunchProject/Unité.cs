425eb93b3401c97b13a105decad5f756f54f4128
DaCosta <luk.3dc@gmail.com>
2021-06-03 13:37:20 +0200

    df

--- KCrunchProject/KCrunchProject/Unité.cs
+++ KCrunchProject/KCrunchProject/Unité.cs
@@ -22,7 +22,7 @@ namespace KCrunchProject
         }
         public Unité(string code)
         {
-            int[] tab = code.Split(':');
+            [] tab = code.Split(':');
 
             CodeU = tab[0];
 
