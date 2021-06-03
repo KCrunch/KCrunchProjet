99e21eee73fd6aa18fefd7bbefa41163ec1a3903
DaCosta <luk.3dc@gmail.com>
2021-06-03 13:51:39 +0200

    Lien chiffreP

--- KCrunchProject/KCrunchProject/Program.cs
+++ KCrunchProject/KCrunchProject/Program.cs
@@ -8,7 +8,7 @@ namespace KCrunchProject
     {
         static void Main(string[] args)
         {
-            île P = new île("C: /Users/Shugo/OneDrive/Bureau/BureauMLK/cours/C#objet/Clerentin/Projet/chiffreP.txt");
+            île P = new île("https://github.com/KCrunch/KCrunchProjet/blob/16ae1dbc55b159a93768c21618c71f842db1c8b1/chiffreP.txt");
         }
     }
 }
