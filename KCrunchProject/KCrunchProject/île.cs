34d80fb3adc54c07afe6a3b95c6d8beac3bd7a74
ShugoTenchi <ShugoTenchi@outlook.fr>
2021-06-03 12:31:00 +0200

    Modif

--- KCrunchProject/KCrunchProject/île.cs
+++ KCrunchProject/KCrunchProject/île.cs
@@ -7,6 +7,35 @@ namespace KCrunchProject
 {
     class île
     {
-        private string Code;
+        private List<Parcelle> code;
+
+        private string chiffreP;
+
+        public île(string cheminAccesFichier)
+        {
+            // instanciations de l'attribut camions
+            code = new List<Parcelle>();
+            // Attention ! La lecture dans le fichier peut échouer
+            // Il faut gérer les erreurs -> structure try...catch
+            try
+            {
+                // Ici, instructions pouvant échouer
+                StreamReader sr = new StreamReader(cheminAccesFichier);
+                string str;
+                Parcelle P;
+                while ((str = sr.ReadLine()) != null)
+                {
+                    P = new Parcelle(str);
+                    code.Add(P);
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                // Exécuté uniquement si erreur dans le bloc try
+                // e est alimentée par Windows avec un message d'erreur
+                Console.WriteLine(e.Message);
+            }
+        }
 
     }
