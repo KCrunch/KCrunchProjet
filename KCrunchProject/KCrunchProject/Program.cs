06e3b0afbcf973cf1c2532ccfbd062572fe1d309
ShugoTenchi <ShugoTenchi@outlook.fr>
2021-06-03 14:22:39 +0200

    test

--- KCrunchProject/KCrunchProject/Program.cs
+++ KCrunchProject/KCrunchProject/Program.cs
@@ -8,9 +8,34 @@ namespace KCrunchProject
     {
         static void Main(string[] args)
         {
-            île P = new île("chiffreP.txt");
-
-            Console.WriteLine(P);
+            
+            string line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("chiffreP.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the lie to console window
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
         }
     }
 }
