24b11980ce902e307c10fa1030dd5406887b89d2
ShugoTenchi <ShugoTenchi@outlook.fr>
2021-06-04 23:37:46 +0200

    :)

--- KCrunchProject/KCrunchProject/Crypter.cs
+++ KCrunchProject/KCrunchProject/Crypter.cs
@@ -11,26 +11,26 @@ namespace KCrunchProject
         private string stockNom;
         private int stockX;
 
-        public Crypter(string NomU, int X, int Y,int code, int Mer, int Forêt, string StockNom, int stockX) : base (NomU,X,Y)
+        public Crypter(string NomU, int X, int Y,int Code, int Mer, int Forêt, string StockNom, int stockX) : base (NomU,X,Y)
         {
             StockNom = NomU;
             for (Y = 0; Y <= 9; Y++)
             {
                 for (X = 0; X <= 9; X++)
                 {
-                    if (NomU == "M")  code = code + Mer; 
-                    if (NomU == "F")  code = code + Forêt; 
-                    if (X == 10) { code = code + 2 ^ 3; }
-                    if (X == 0) { code = code + 2 ^ 1; }
-                    if (Y == 10) { code = code + 2 ^ 2; }
-                    if (Y == 0) { code = code + 2 ^ 0; }
-                    if (X == 10 && Y == 10) { code = code + 2 ^ 3 + 2 ^ 2; }
-                    if (X == 0 && Y == 0) { code = code + 2 ^ 0 + 2 ^ 1; }
-                    if (X == 0 && Y == 10) { code = code + 2 ^ 1 + 2 ^ 2; }
-                    if (X == 10 && Y == 0) { code = code + 2 ^ 0 + 2 ^ 3; }
+                    if (NomU == "M")  Code = Code + Mer; 
+                    if (NomU == "F")  Code = Code + Forêt; 
+                    if (X == 10) { Code = Code + 2 ^ 3; }
+                    if (X == 0) { Code = Code + 2 ^ 1; }
+                    if (Y == 10) { Code = Code + 2 ^ 2; }
+                    if (Y == 0) { Code = Code + 2 ^ 0; }
+                    if (X == 10 && Y == 10) { Code = Code + 2 ^ 3 + 2 ^ 2; }
+                    if (X == 0 && Y == 0) { Code = Code + 2 ^ 0 + 2 ^ 1; }
+                    if (X == 0 && Y == 10) { Code = Code + 2 ^ 1 + 2 ^ 2; }
+                    if (X == 10 && Y == 0) { Code = Code + 2 ^ 0 + 2 ^ 3; }
                     if (StockNom != NomU) 
                     { 
-                        if(X == X +1) { code = code + 2 ^ 3; }
+                        if(X == X +1) { Code = Code + 2 ^ 3; }
                     }
                     StockNom = NomU;
                 }
--- KCrunchProject/KCrunchProject/île.cs
+++ KCrunchProject/KCrunchProject/île.cs
@@ -65,15 +65,40 @@ namespace KCrunchProject
             }
 
         }
+
+        /*
         public void Crypter()
         {
-
+            string StockNom = NomU;
+            for (Y = 0; Y <= 9; Y++)
+            {
+                for (X = 0; X <= 9; X++)
+                {
+                    if (NomU == "M") Code = Code + Mer;
+                    if (NomU == "F") Code = Code + Forêt;
+                    if (X == 10) { Code = Code + 2 ^ 3; }
+                    if (X == 0) { Code = Code + 2 ^ 1; }
+                    if (Y == 10) { Code = Code + 2 ^ 2; }
+                    if (Y == 0) { Code = Code + 2 ^ 0; }
+                    if (X == 10 && Y == 10) { Code = Code + 2 ^ 3 + 2 ^ 2; }
+                    if (X == 0 && Y == 0) { Code = Code + 2 ^ 0 + 2 ^ 1; }
+                    if (X == 0 && Y == 10) { Code = Code + 2 ^ 1 + 2 ^ 2; }
+                    if (X == 10 && Y == 0) { Code = Code + 2 ^ 0 + 2 ^ 3; }
+                    if (StockNom != NomU)
+                    {
+                        if (X == X + 1) { Code = Code + 2 ^ 3; }
+                    }
+                    StockNom = NomU;
+                }
+            }
         }
 
         public void Decrypter()
         {
 
         }
+        */
+
         #endregion
     }
 }
