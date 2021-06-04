1bfa6f97479fe44cf90b77a648bf01c434c29c1c
ShugoTenchi <ShugoTenchi@outlook.fr>
2021-06-04 16:55:34 +0200

    Revert "Indentation fix"
    This reverts commit 4d265c40f0352ff3cd148d738367af0ea8810909.

--- KCrunchProject/KCrunchProject/(Dé)crypteur.cs
+++ KCrunchProject/KCrunchProject/(Dé)crypteur.cs
@@ -13,15 +13,26 @@ namespace KCrunchProject
             int Forêt = 32;
             int Code = 0;
 
-            for (int i = 0; i <= 9; i++) 
-            {
+            for (int i = 0; i <= 9; i++) {
                 for (int j = 0; j <= 9; i++)
                 {
+
+                }
+            }
+            
+                
+                
                     if (X = j + 1 && Y = i)&& (NomU != F || M))
                     {
-                        if (X = 10 && Y = if && (NomU != F || M)) { Code = 2 ^ 3; }
-                        else { Code = 2 ^ 3; }
-                    }       
+                        if (X = 10 && Y = if && (NomU != F || M))
+                        {
+                            Code = 2 ^ 3;
+                        }
+                        else
+                        {
+                            Code = 2 ^ 3;
+                        }
+                    }
         }
     }
 }
