04121b71b0d038c57ce776de66cf474240d567ae
ShugoTenchi <ShugoTenchi@outlook.fr>
2021-06-04 22:12:57 +0200

    Modif

--- KCrunchProject/KCrunchProject/Crypter.cs
+++ KCrunchProject/KCrunchProject/Crypter.cs
@@ -2,35 +2,35 @@
 using System.Collections.Generic;
 using System.Text;
 
-/*namespace KCrunchProject
+namespace KCrunchProject
 {
-  /*  class Crypter : Unité
+    class Crypter : Unité
     {
         private int Mer = 64;
         private int Forêt = 32;
-        private char stockNom;
+        private string stockNom;
         private int stockX;
 
-        public Crypter(int X, int Y, char NomU, int Code, int Mer, int Forêt, char StockNom, int stockX) : base (X,Y,NomU)
+        public Crypter(string NomU, int X, int Y,  int code, int Mer, int Forêt, string StockNom, int stockX) : base (NomU,X,Y,code)
         {
             StockNom = NomU;
             for (Y = 0; Y <= 9; Y++)
             {
                 for (X = 0; X <= 9; X++)
                 {
-                    if (NomU == 'M') { Code = Code + Mer; }
-                    if (NomU == 'F') { Code = Code + Forêt; }
-                    if (X == 10) { Code = Code + 2 ^ 3; }
-                    if (X == 0) { Code = Code + 2 ^ 1; }
-                    if (Y == 10) { Code = Code + 2 ^ 2; }
-                    if (Y == 0) { Code = Code + 2 ^ 0; }
-                    if (X == 10 && Y == 10) { Code = Code + 2 ^ 3 + 2 ^ 2; }
-                    if (X == 0 && Y == 0) { Code = Code + 2 ^ 0 + 2 ^ 1; }
-                    if (X == 0 && Y == 10) { Code = Code + 2 ^ 1 + 2 ^ 2; }
-                    if (X == 10 && Y == 0) { Code = Code + 2 ^ 0 + 2 ^ 3; }
+                    if (NomU == "M") { code = code + Mer; }
+                    if (NomU == "F") { code = code + Forêt; }
+                    if (X == 10) { code = code + 2 ^ 3; }
+                    if (X == 0) { code = code + 2 ^ 1; }
+                    if (Y == 10) { code = code + 2 ^ 2; }
+                    if (Y == 0) { code = code + 2 ^ 0; }
+                    if (X == 10 && Y == 10) { code = code + 2 ^ 3 + 2 ^ 2; }
+                    if (X == 0 && Y == 0) { code = code + 2 ^ 0 + 2 ^ 1; }
+                    if (X == 0 && Y == 10) { code = code + 2 ^ 1 + 2 ^ 2; }
+                    if (X == 10 && Y == 0) { code = code + 2 ^ 0 + 2 ^ 3; }
                     if (StockNom != NomU) 
                     { 
-                        if(X == X +1) { Code = Code + 2 ^ 3; }
+                        if(X == X +1) { code = code + 2 ^ 3; }
                     }
                     StockNom = NomU;
                 }
