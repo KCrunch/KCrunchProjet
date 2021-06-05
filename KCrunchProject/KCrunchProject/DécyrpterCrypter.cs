cdc70899ff5fab752936d068e8070f23357ad1cd
ShugoTenchi <ShugoTenchi@outlook.fr>
2021-06-06 00:35:44 +0200

    Rajout fonction créee fichier

--- KCrunchProject/KCrunchProject/DécyrpterCrypter.cs
+++ KCrunchProject/KCrunchProject/DécyrpterCrypter.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace KCrunchProject
-{ 
-    static class DécyrpterCrypter 
+{
+    static class DécyrpterCrypter
     {
         const int Mer = 64;
         const int Foret = 32;
@@ -16,26 +17,28 @@ namespace KCrunchProject
 
         static public void Crypter(Ile Ile)
         {
+            string Fichier="";
             foreach (Unité U in Ile.Unités)
             {
-                U.Code += SommeNordOuestSudEst(U.NomU, U.X, U.Y, Ile.Unités);
                 U.Code += VerifMerOuForet(U.NomU);
-                U.Code += FrontiereExeption(U.X,U.Y); ;
-                AfficheCrypt(U.Code, U.X);
-            }  
+                U.Code += FrontiereExeption(U.X, U.Y); ;
+                U.Code += SommeNordOuestSudEst(U.NomU, U.X, U.Y, Ile.Unités);
+                Fichier += AfficheCrypt(U.Code, U.X);
+            }
+            CreeFichierCrypter(Fichier, "../../../Phatt.chiffre.txt");
         }
 
         static public int VerifMerOuForet(char nomU)
         {
-            int code=0;
-            if (nomU == 'M') 
+            int code = 0;
+            if (nomU == 'M')
                 code = code + Mer;
-            else if (nomU == 'F') 
+            else if (nomU == 'F')
                 code = code + Foret;
             return code;
         }
 
-        static public int FrontiereExeption(int X,int Y)
+        static public int FrontiereExeption(int X, int Y)
         {
             int code = 0;
             if (X == 9) code = code + Est;
@@ -49,37 +52,49 @@ namespace KCrunchProject
             int code = 0;
             foreach (Unité U in ListeUnites)
             {
-                
-                if (U.NomU != nom && U.Y + 1 == BoiteY  && U.X == BoiteX)
-                {
+
+                if (U.NomU != nom && U.Y + 1 == BoiteY && U.X == BoiteX)
                     code = code + Nord;
-                }
-                else if (U.NomU != nom && U.X - 1 == BoiteX  && U.Y == BoiteY)
-                {
+                else if (U.NomU != nom && U.X + 1 == BoiteX && U.Y == BoiteY)
                     code = code + Ouest;
-                }
-                else if (U.NomU != nom && U.Y - 1 == BoiteY  && U.X == BoiteX)
-                {
+                else if (U.NomU != nom && U.Y - 1 == BoiteY && U.X == BoiteX)
                     code = code + Sud;
-                }
-                else if (U.NomU != nom && U.X + 1 == BoiteX  && U.Y == BoiteY)
-                {
+                else if (U.NomU != nom && U.X - 1 == BoiteX && U.Y == BoiteY)
                     code = code + Est;
-                }
             }
             return code;
         }
 
-        static public void AfficheCrypt(int code, int x)
+        static public void CreeFichierCrypter(string Fichier, string cheminAccesFichier) 
         {
+            if (File.Exists(cheminAccesFichier))
+            {
+                File.Delete(cheminAccesFichier);
+            }
+
+            // Créer un nouveau fichier   
+            using (FileStream fileStr = File.Create(cheminAccesFichier))
+            {
+                // Ajouter du texte au fichier  
+                Byte[] text = new UTF8Encoding(true).GetBytes(Fichier);
+                fileStr.Write(text, 0, text.Length);
+            }
+        }
+
+        static public string AfficheCrypt(int code, int x)
+        {
+            string FichierCrypter;
             if (x >= 9)
             {
-                Console.Write("{0}|",code);
+                Console.Write("{0}|", code);
+                FichierCrypter = Convert.ToString(code) + "|";
             }
-            else 
+            else
+            {
                 Console.Write("{0}:", code);
-
-
+                FichierCrypter = Convert.ToString(code) + ":";
+            }
+            return FichierCrypter;
         }
         /*
         public void Décrypter ()
